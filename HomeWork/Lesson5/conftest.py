23c8dfb3d220d7244e2feee22a9517dd5b0b11a3
Keanmair <keanmair@gmail.com>
2019-04-10 14:31:20 +0300

    Homework selenium

--- HomeWork/Lesson5/conftest.py
+++ HomeWork/Lesson5/conftest.py
@@ -1,37 +1,54 @@
 import pytest
 from selenium import webdriver
-from webdrivermanager import ChromeDriverManager
+import logging
+
+# __logger = logging.getLogger('Lololo')
+# __logger.setLevel('INFO')
 
 
-@pytest.fixture
-def chrome_browser(request):
+def __str2bool(v):
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise TypeError('Boolean value expected.')
+
+
+def chrome_browser():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--start-maximized")
+    options.headless = True
     chromedriver = "/home/korneev/Drivers/chromedriver"
-    # wd = webdriver.Chrome(ChromeDriverManager().download_and_install(), options=options)
-    driver = webdriver.Chrome(executable_path=chromedriver)
-    # wd.implicitly_wait()
-    # wd.set_page_load_timeout()
+    driver = webdriver.Chrome(executable_path=chromedriver, options=options)
     driver.maximize_window()
-    request.addfinalizer(driver.quit)
     return driver
+
+
+def firefox_browser():
+    options = webdriver.FirefoxOptions()
+    options.headless = True
+    firefoxdriver = "/home/korneev/Drivers/geckodriver"
+    driver = webdriver.Firefox(executable_path=firefoxdriver, options=options)
+    driver.maximize_window()
+    return driver
+
+
+def pytest_addoption(parser):
+    parser.addoption("--brw", action="store", default="c", help="My option: c for Chrome, f for Firefox")
+    parser.addoption("--url", action="store", type=__str2bool, default=False, help="My option: True or False")
+
 
 @pytest.fixture
-def firefox_browser(request):
-    options = webdriver.FirefoxOptions()
-    options.add_argument("--headless")
-    options.add_argument("--start-maximized")
-    firefoxdriver = "/home/korneev/Drivers/geckodriver"
-    driver = webdriver.Firefox(executable_path=firefoxdriver)
-    driver.maximize_window()
-    request.addfinalizer(driver.quit)
-    return driver
+def brw(request):
+    if request.config.getoption("--url"):
+        print("\nBase URL: http://192.168.77.43/opencart/")
+        # __logger.info("http://192.168.77.43/opencart/")
 
-def test_chrome(chrome_browser):
-    chrome_browser.get("http://192.168.77.43/opencart//")
-    assert chrome_browser.find_element_by_class_name('col-sm-4').text == 'Your Store'
+    if request.config.getoption("--brw") == "f":
+        drv = firefox_browser()
+    if request.config.getoption("--brw") == "c":
+        drv = chrome_browser()
+    request.addfinalizer(drv.quit)
+    return drv
 
-def test_firefox(firefox_browser):
-    firefox_browser.get(("http://192.168.77.43/opencart//"))
-    assert chrome_browser.find_element_by_class_name('col-sm-4').text == 'Your Store'
+# baseUrl == "http://192.168.77.43/opencart/"
