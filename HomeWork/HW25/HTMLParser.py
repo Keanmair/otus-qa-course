3876f931800d7d314a1fcddfa1041db710e7dcc7
korneev <Keanmair@gmail.com>
2019-08-01 17:20:01 +0300

    HW25

---
+++ HomeWork/HW25/HTMLParser.py
@@ -0,0 +1,33 @@
+from collections import Counter
+from html.parser import HTMLParser
+
+
+class HTMLParser(HTMLParser):
+
+    def __init__(self):
+        super().__init__()
+        self.tags = Counter()
+        self.text = []
+        self.links = []
+
+    def handle_starttag(self, tag, attrs):
+        self.tags[tag] += 1
+        for attr in attrs:
+            if attr[0] == "href":
+                self.links.append(attr[1])
+            if tag == "img" and attr[0] == "src":
+                self.links.append(attr[1])
+
+    def handle_data(self, data):
+        strip_data = data.strip()
+        if strip_data != "":
+            self.text.append(strip_data)
+
+    def get_text(self):
+        return self.text
+
+    def get_tags(self):
+        return self.tags
+
+    def get_links(self):
+        return self.links
---
+++ HomeWork/HW25/app.py
@@ -0,0 +1,18 @@
+from HomeWork.HW25.client import HttpClient
+
+if __name__ == '__main__':
+    req_method = "GET"
+    req_host = "demo23.opencart.pro"
+    req_port = 80
+    req_url = "/admin/"
+    req_headers = {"Host": "demo23.opencart.pro"}
+    client = HttpClient()
+    client.send_request(req_method, req_host, req_port, req_url, req_headers)
+    print("Start line: {}".format(client.get_start_line()))
+    print("Headers: {}".format(client.get_headers()))
+    print("Body: {}".format(client.get_body()))
+    print("Part2:")
+    print("Tag list: {}".format(client.get_tag_list()))
+    print("Page text: {}".format(client.get_text()))
+    print("Most frequent tag: {}".format(client.get_most_frequent_tag()))
+    print("Page links: {}".format(client.get_links()))
